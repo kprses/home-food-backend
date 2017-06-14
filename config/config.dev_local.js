26013847169eaaa9bcd803229dac53b615b2da1b
Kenan <kenan.prses@tum.de>
2017-06-14 16:20:41 +0200

    initial commit

---
+++ config/config.dev_local.js
@@ -0,0 +1,15 @@
+var Config = {};
+Config.db = {};
+Config.app={};
+Config.auth = {};
+
+Config.db.host = 'localhost:27017';
+Config.db.name = 'moviedb';
+
+// Use environment defined port or 3000
+//Config.app.port = process.env.PORT || 3000;
+Config.app.port = 3001;
+
+Config.auth.jwtSecret = "very secret secret";
+
+module.exports = Config;
