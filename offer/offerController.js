3e3db42dfcfcd1f665a1eedcead5c7798455557f
Kenan <kenan.prses@tum.de>
2017-06-16 01:56:24 +0200

    offerControler cleaned

--- offer/offerController.js
+++ offer/offerController.js
@@ -15,57 +15,6 @@ exports.createOffer = function(req, res) {
     });
 };
 
-/*
-// Create endpoint /api/movies for GET
-exports.getMovies = function(req, res) {
-    Movie.find(function(err, movies) {
-        if (err) {
-            res.status(400).send(err);
-            return;
-        }
-        res.json(movies);
-    });
-};
-// Create endpoint /api/movies/:movie_id for GET
-exports.getMovie = function(req, res) {
-    // Use the Movie model to find a specific movie
-    Movie.findById(req.params.movie_id, function(err, movie) {
-        if (err) {
-            res.status(400).send(err)
-            return;
-        };
-
-        res.json(movie);
-    });
-};
-// Create endpoint /api/movies/:movie_id for PUT
-exports.putMovie = function(req, res) {
-    // Use the Movie model to find a specific movie and update it
-    Movie.findByIdAndUpdate(
-        req.params.movie_id,
-        req.body, {
-            //pass the new object to cb function
-            new: true,
-            //run validations
-            runValidators: true
-        },
-        function(err, movie) {
-            if (err) {
-                res.status(400).send(err);
-                return;
-            }
-            res.json(movie);
-        });
-};
-// Create endpoint /api/movies/:movie_id for DELETE
-exports.deleteMovie = function(req, res) {
-    // Use the Beer model to find a specific beer and remove it
-    Movie.findById(req.params.movie_id, function(err, m) {
-        if (err) {
-            res.status(400).send(err);
-            return;
-        }
-        m.remove();
-        res.sendStatus(200);
-    });
-};*/
+// TO DO:
+//GET ALL, GET BY ID, UPDATE, AND DELETE 
+// ANALOGOUS TO THE MOVIE APP CRUD
